f23628e807eae4ede7352f2d3735ce135899cda6
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2025-05-16 14:02:31 -0700

    n

--- Libraries/UI/Client.lua
+++ Libraries/UI/Client.lua
@@ -2,18 +2,22 @@ local cloneref = cloneref and cloneref or function(...) return ... end
 local gethui = (gethui and gethui()) or cloneref(game:GetService("CoreGui")) 
 
 local players = cloneref(game:GetService("Players"))
+local runService = cloneref(game:GetService("RunService"))
+
 local localPlayer = players.LocalPlayer 
+local camera = workspace.CurrentCamera 
+local viewportSize = camera.ViewportSize 
 
 local findFirstChild = workspace.FindFirstChild
 
 local library = {
-    {},
-    {
+    instances = {},
+    client = {
         fps = 60,
         ping = 100,
         mem = 200
     },
-    {
+    localPlayer = {
         name = localPlayer.DisplayName,
         vel = 0,
         pos = {0, 0, 0},
@@ -21,7 +25,7 @@ local library = {
         build = "user",
         version = "1.0.0"
     },
-    {
+    closestPlayer = {
         name = "",
         health = 100,
         vel = 0,
@@ -30,8 +34,9 @@ local library = {
         vis = false,
         friendly = false 
     },
-    {
-        font = "",
+    settings = {
+        font,
+        size = 13,
         image = "",
         gradient = { },
         accent = Color3.fromRGB(255, 100, 100),
@@ -43,7 +48,7 @@ function library:create(instance, properties)
     local obj = Instance.new(instance)
 
     for i, v in pairs(properties) do
-        obj[string.upper(string.sub(i, 1, 1)) .. string.sub(i, 2)] = v
+        obj[i] = v
     end
 
     self[1][#self[1] + 1] = obj
@@ -64,12 +69,12 @@ function library:isAlive(player)
 
         if root and humanoid then 
             if humanoid.Health > 0 then 
-                return player, char, root, humanoid 
+                return true, player, char, root, humanoid 
             end 
         end 
     end 
 
-    return nil, nil, nil, nil 
+    return false, nil, nil, nil, nil 
 end 
 
 function library:isFriendly(player)
@@ -86,46 +91,76 @@ function library:randomString(name)
     return name .. result
 end
 
+if not isfile("proggyclean.ttf") then 
+    writefile("proggyclean.ttf", game:HttpGet("https://github.com/Streekaiz/assets/raw/refs/heads/main/Fonts/ProggyTiny.ttf"))
+end 
+    
+if not isfile("proggyclean_encoded.ttf") then 
+    writefile("proggyclean_encoded.ttf", game:GetService("HttpService"):JSONEncode({ 
+        name = "ProggyTiny", 
+        faces = { 
+            { 
+                name = "Regular", 
+                weight = 400, 
+                style = "normal", 
+                assetId = getcustomasset("proggyclean.ttf") 
+            } 
+        } 
+    }))
+end 
+
+library[5].font = Font.new(getcustomasset("proggyclean_encoded.ttf"), Enum.FontWeight.Regular)
+
 local screenGui = library:create("ScreenGui", {
-    name = library:randomString("screenGui"),
-    parent = gethui()
+    Name = library:randomString("screenGui"),
+    Parent = gethui()
 })
 
-local title = library:create("Text", {
-    name = library:randomString("title"),
-    text = library[3].title .. " | build: " .. library[3].build  .. " | v" .. library[3].version,
-    parent = screenGui 
+local title = library:create("TextLabel", {
+    Name = library:randomString("title"),
+    Text = library[3].title .. " | build: " .. library[3].build  .. " | v" .. library[3].version,
+    FontFace = library[5].font,
+    Parent = screenGui 
 })
 
-local content = library:create("Text", {
-    name = library:randomString("content"),
-    text = [[
+local content = library:create("TextLabel", {
+    Name = library:randomString("content"),
+    FontFace = library[5].font, 
+    Text = [[
         local player
         > username: 
         > velocity:
         > position:
     ]],
-    parent = screenGui 
+    Parent = screenGui 
 })
 
-local closestPlayerContent = library:create("Text", {
-    name = library:randomString("closestPlayerContent"),
-    text = [[
+local closestPlayerContent = library:create("TextLabel", {
+    Name = library:randomString("closestPlayerContent"),
+    FontFace = library[5].font,
+    Text = [[
         closest player
         > username:
         > velocity:
         > position:
         > distance:
     ]],
-    parent = screenGui
+    Parent = screenGui
 })
 
 local titleGradient = library:create("UIGradient", {
-    parent = title,
-    name = library:randomString("titleGradient"),
-    color = ColorSequence.new{
+    Parent = title,
+    Name = library:randomString("titleGradient"),
+    Color = ColorSequence.new{
         ColorSequenceKeypoint.new(0, library[5].text),
         ColorSequenceKeypoint.new(0.01, library[5].accent),
         ColorSequenceKeypoint.new(1, library[5].text)
     }
 })
+
+library.connection = runService.RenderStepped:Connect(function(delta)
+    local player, char, root, humanoid = library:isAlive(localPlayer)
+    if player and root then 
+        content.Text = string.format("local player\n> name: %s\n> velocity: %s\n>position: %s", player.Name, tostring(root.Velocity), tostring(root.Position))
+    end 
+end)
